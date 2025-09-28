122f972a988e06fcbb77cb8e72a3efa05c87e49c
Rama Almer Felix <86484270+quenzvezda@users.noreply.github.com>
2025-09-28 10:37:11 +0700

    Improve avatar cropper UX

--- src/types/react-easy-crop.d.ts
+++ src/types/react-easy-crop.d.ts
@@ -13,20 +13,41 @@ declare module 'react-easy-crop' {
     y: number
   }
 
+  export interface Size {
+    width: number
+    height: number
+  }
+
+  export interface MediaSize extends Size {
+    naturalWidth: number
+    naturalHeight: number
+  }
+
+  export interface CropperStyle {
+    containerStyle?: React.CSSProperties
+    mediaStyle?: React.CSSProperties
+    cropAreaStyle?: React.CSSProperties
+  }
+
   export interface CropperProps {
     image: string
     crop: Point
     zoom: number
     aspect?: number
+    minZoom?: number
+    maxZoom?: number
+    cropShape?: 'rect' | 'round'
     onCropChange?: (location: Point) => void
     onZoomChange?: (zoom: number) => void
     onCropComplete?: (croppedArea: Area, croppedAreaPixels: Area) => void
+    onMediaLoaded?: (mediaSize: MediaSize) => void
     objectFit?: 'contain' | 'cover'
     zoomWithScroll?: boolean
+    style?: CropperStyle
   }
 
   const Cropper: React.FC<CropperProps>
 
   export default Cropper
-  export type { Area }
+  export type { Area, MediaSize }
 }
